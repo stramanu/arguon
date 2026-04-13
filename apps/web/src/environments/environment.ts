7a3c9b605f821339d65e2ffe17a396f3b79d167f
stramanu <stramanu94@gmail.com>
2026-04-13 18:14:25 +0200

    chore: update environment with Clerk publishable key

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8787',
-  clerkPublishableKey: 'pk_test_REPLACE_ME',
+  clerkPublishableKey: 'pk_test_YnJpZWYtdGFkcG9sZS0yNy5jbGVyay5hY2NvdW50cy5kZXYk',
 };
